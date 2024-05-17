c237c0f0e87c7790590be1c53ec36cd1e1e42b11
Toddy Mladenov <toddysm@gmail.com>
2024-05-17 10:59:51 -0700

    Changed pod names
    Signed-off-by: Toddy Mladenov <toddysm@gmail.com>

--- demos/04-deploy-image.sh
+++ demos/04-deploy-image.sh
@@ -5,11 +5,11 @@ kubectl get pods
 
 # 2. Deploy the unsigned image
 
-kubectl run demo-app --image-pull-policy='Always' --image=acrbuild2024.azurecr.io/flaskapp:1.0-unsigned
+kubectl run demo-app-unsigned --image-pull-policy='Always' --image=acrbuild2024.azurecr.io/flaskapp:1.0-unsigned
 kubectl get pods
 
 # 3. Copy the signed image to GHCR
 oras cp -r acrbuild2024.azurecr.io/flaskapp:1.0 ghcr.io/payalmahesh/flaskapp:1.0
 
 # 4. Deploy image from anapproved registry
-kubectl run demo-app --image-pull-policy='Always' --image=ghcr.io/payalmahesh/flaskapp:1.0
+kubectl run demo-app-unapproved --image-pull-policy='Always' --image=ghcr.io/payalmahesh/flaskapp:1.0
